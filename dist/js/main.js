ef4fc798ccfbf35c1cacfd9b3625382139b332bb
liangxiaoming <398921432@qq.com>
2017-09-29 23:52:13 +0800

    done 2

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,5 +2,5 @@
  * @project : MidAutumnFestival
  * @version : 0.0.1
  * @author  : 
- * @update  : 2017-09-29 6:23:08 pm
+ * @update  : 2017-09-29 11:52:02 pm
  */!function(r){function t(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var e={};return t.m=r,t.c=e,t.p="./js/",t(0)}([function(r,t){}]);
